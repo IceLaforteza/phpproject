8db78a5a7cc35de0d04f3f254a74e1ae573a3bb1
Ice <IceLaforteza29@gmail.com>
2023-04-03 11:01:27 +0200

    ice profilepage

---
+++ profilepage.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User-profile</title>
+    <link rel="stylesheet" href="profilepage.css">
+
+</head>
+<body>
+<div class="mom">
+    <div class="images">
+        <div class="banner">
+            <img src="" alt="">
+        </div>
+    </div>
+</div>
+
+<main id="tweetsETC">
+
+</main>
+</body>
+</html>
+
+<?php
